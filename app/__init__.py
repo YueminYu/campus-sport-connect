836e7675644954dc37f19645837d8c2f1806a2df
Yuemin Yu <yueminy2@illinois.edu>
2024-10-04 16:00:42 -0500

    Web main page, login, and signup pages are built up

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'  # Use a secure key
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from .models import User  # Import User model after app is created
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    from .routes import main_routes
+    app.register_blueprint(main_routes)
+
+    return app
---
+++ app/models.py
@@ -0,0 +1,21 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    preferred_sport = db.Column(db.String(50), nullable=True)
+
+    events = db.relationship('Event', backref='creator', lazy=True)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sport_type = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(20), nullable=False)
+    time = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    max_participants = db.Column(db.Integer, nullable=False)
+    current_participants = db.Column(db.Integer, default=0)
